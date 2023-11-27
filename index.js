0bfd443479fb920092f52b37f625e3390d6f1959
Markobanduka <banduka44@gmail.com>
2023-11-27 19:47:47 +0400

    fixing restarting result

--- index.js
+++ index.js
@@ -41,70 +41,144 @@ function enableNumbers(){
   nine.disabled = false;
   zero.disabled = false;
 }
+let restartedResult= false;
+
 const one = document.querySelector("#one");
 one.addEventListener("click", function () {
   enableOperations();
+  if(restartedResult){
+    result ="";
+    result +=1;
+    input.value= result;
+    restartedResult =false;
+  } else{
   result += 1;
   input.value = result;
+}
+
 });
 const two = document.querySelector("#two");
 two.addEventListener("click", function () {
   enableOperations();
+  if(restartedResult){
+    result ="";
+    result +=2;
+    input.value= result;
+    restartedResult =false;
+  } else{
   result += 2;
   input.value = result;
+}
 });
 const three = document.querySelector("#three");
 three.addEventListener("click", function () {
   enableOperations();
+  if(restartedResult){
+    result ="";
+    result += 3;
+    input.value= result;
+    restartedResult =false;
+  } else{
   result += 3;
   input.value = result;
+}
 });
 const four = document.querySelector("#four");
 four.addEventListener("click", function () {
-  enableOperations();
+  if(restartedResult){
+    result ="";
+    result += 4;
+    input.value= result;
+    restartedResult =false;
+  } else{
   result += 4;
+  input.value = result;
+}
   input.value = result;
 });
 const five = document.querySelector("#five");
 five.addEventListener("click", function () {
   enableOperations();
+  if(restartedResult){
+    result ="";
+    result += 5;
+    input.value= result;
+    restartedResult =false;
+  } else{
   result += 5;
   input.value = result;
+}
 });
 const six = document.querySelector("#six");
 six.addEventListener("click", function () {
   enableOperations();
+  if(restartedResult){
+    result ="";
+    result += 6;
+    input.value= result;
+    restartedResult =false;
+  } else{
   result += 6;
   input.value = result;
+}
 });
 const seven = document.querySelector("#seven");
 seven.addEventListener("click", function () {
   enableOperations();
+  if(restartedResult){
+    result ="";
+    result += 7;
+    input.value= result;
+    restartedResult =false;
+  } else{
   result += 7;
   input.value = result;
+}
 });
 const eight = document.querySelector("#eight");
 eight.addEventListener("click", function () {
   enableOperations();
+  if(restartedResult){
+    result ="";
+    result += 8;
+    input.value= result;
+    restartedResult =false;
+  } else{
   result += 8;
   input.value = result;
+}
 });
 const nine = document.querySelector("#nine");
 nine.addEventListener("click", function () {
   enableOperations();
+  if(restartedResult){
+    result ="";
+    result += 9;
+    input.value= result;
+    restartedResult =false;
+  } else{
   result += 9;
   input.value = result;
+}
 });
 const zero = document.querySelector("#zero");
 zero.addEventListener("click", function () {
   enableOperations();
+  if(restartedResult){
+    result ="";
+    result += 0;
+    input.value= result;
+    restartedResult =false;
+  } else{
   result += 0;
   input.value = result;
+}
 });
 const divide = document.querySelector("#divide");
 divide.addEventListener("click", function () {
   disableOperations();
   enableNumbers();
+  restartedResult= false;
   result += "/";
   input.value = result;
 });
@@ -112,6 +186,7 @@ const multiply = document.querySelector("#multiply");
 multiply.addEventListener("click", function() {
   disableOperations();
   enableNumbers();
+  restartedResult= false;
   result += "*";
   input.value = result;
 });
@@ -119,6 +194,7 @@ const minus = document.querySelector("#minus");
 minus.addEventListener("click", function () {
   disableOperations();
   enableNumbers();
+  restartedResult= false;
   result += "-";
   input.value = result;
 });
@@ -126,14 +202,16 @@ const plus = document.querySelector("#plus");
 plus.addEventListener("click", function () {
   disableOperations();
   enableNumbers();
+  restartedResult= false;
   result += "+";
   input.value = result;
 });
 const equals = document.querySelector("#equals");
 equals.addEventListener("click", function () {
-  disableNumbers();
   result = eval(result);
   input.value = result;
+  restartedResult =true;
+  
 });
 const clear = document.querySelector("#clear");
 clear.addEventListener("click", function () {
